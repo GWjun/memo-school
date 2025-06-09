3ba8abde663dadec9966a966085d7b62a30aa196
GWjun <kb865900@gmail.com>
2025-06-09 16:37:28 +0900

    feat: DsCon beans

---
+++ src/db/DsCon.java
@@ -0,0 +1,20 @@
+package myBean.db;
+
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.*;
+
+public class DsCon {
+
+  /**
+   * Context로부터 커넥션 풀에 접근하여 Connection 을 가져온다.
+   *
+   * @return Connection
+   * @throws SQLException, NamingException
+   */
+  public static Connection getConnection() throws NamingException, SQLException {
+    Context initContext = new InitialContext();
+    DataSource ds = (DataSource) initContext.lookup("java:/comp/env/jdbc/mydbTest");
+    return ds.getConnection();
+  }
+}
